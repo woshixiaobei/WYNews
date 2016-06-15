738221ba5e3efd3d8fba08fe551f7df812f3b522
manager <manager@qq.com>
2016-06-15 19:50:39 +0800

    新建列表目录,新建新闻列表控制器

---
+++ WYNews/WYNews/Class/Home/NewsList/WYNewsListController.m
@@ -0,0 +1,27 @@
+//
+//  WYNewsListController.m
+//  WYNews
+//
+//  Created by 小贝 on 16/6/15.
+//  Copyright © 2016年 小贝. All rights reserved.
+//
+
+#import "WYNewsListController.h"
+
+@interface WYNewsListController ()
+
+@end
+
+@implementation WYNewsListController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+
+
+
+@end
