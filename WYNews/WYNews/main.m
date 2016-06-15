be2c88dccef9ee9da2ba14ec094bc8ce3bf4c020
manager <manager@qq.com>
2016-06-15 16:18:31 +0800

    添加第三方框架,删除pods的警号,删除main/cv实现准备项目工作

---
+++ WYNews/WYNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WYNews
+//
+//  Created by 小贝 on 16/6/15.
+//  Copyright © 2016年 小贝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
