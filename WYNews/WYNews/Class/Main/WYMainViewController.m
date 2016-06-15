a2fece0e703f7c636d8afe527d83559434ff25eb
manager <manager@qq.com>
2016-06-15 19:35:32 +0800

    实现添加多个子控制器

--- WYNews/WYNews/Class/Main/WYMainViewController.m
+++ WYNews/WYNews/Class/Main/WYMainViewController.m
@@ -24,11 +24,11 @@
 - (void)addChildViewControllers {
     
     NSArray *array = @[
-                       @{@"clsName":@"UIViewController",@"title":@"首页",@"imageName":@"tabbar_icon_bar_normal"},
-                       @{@"clsName":@"UIViewController",@"title":@"阅读",@"imageName":@"tabbar_icon_bar_normal"},
-                       @{@"clsName":@"UIViewController",@"title":@"视频",@"imageName":@"tabbar_icon_bar_normal"},
-                       @{@"clsName":@"UIViewController",@"title":@"话题",@"imageName":@"tabbar_icon_bar_normal"},
-                       @{@"clsName":@"UIViewController",@"title":@"我",@"imageName":@"tabbar_icon_bar_normal"}
+                       @{@"clsName":@"UIViewController",@"title":@"首页",@"imageName":@"news"},
+                       @{@"clsName":@"UIViewController",@"title":@"阅读",@"imageName":@"reader"},
+                       @{@"clsName":@"UIViewController",@"title":@"视频",@"imageName":@"media"},
+                       @{@"clsName":@"UIViewController",@"title":@"话题",@"imageName":@"bar"},
+                       @{@"clsName":@"UIViewController",@"title":@"我",@"imageName":@"me"}
                        ];
     NSMutableArray *arrayM = [NSMutableArray array];
     for ( NSDictionary *dict in array) {
@@ -54,8 +54,14 @@
     
     vc.title = dict[@"title"];
 
-    NSString *imageName = dict[@"imageName"];
+    NSString *imageName = [NSString stringWithFormat:@"tabbar_icon_%@_normal",dict[@"imageName"]];
+    //NSString *imageName = dict[@"imageName"];
     vc.tabBarItem.image = [UIImage imageNamed:imageName];
+    
+    //设置高亮状态
+    NSString *imageNameHL = [NSString stringWithFormat:@"tabbar_icon_%@_highlight",dict[@"imageName"]];
+    vc.tabBarItem.selectedImage = [[UIImage imageNamed:imageNameHL] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];;
+    
     
     
     UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:vc];
