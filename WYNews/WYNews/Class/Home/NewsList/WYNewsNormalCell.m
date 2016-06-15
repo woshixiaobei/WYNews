0fe9d3dad6cdbe5259c1eb78645764f5f1147b85
manager <manager@qq.com>
2016-06-15 22:53:54 +0800

    自定义mormal 的cell

---
+++ WYNews/WYNews/Class/Home/NewsList/WYNewsNormalCell.m
@@ -0,0 +1,24 @@
+//
+//  WYNewsNormalCell.m
+//  WYNews
+//
+//  Created by 小贝 on 16/6/15.
+//  Copyright © 2016年 小贝. All rights reserved.
+//
+
+#import "WYNewsNormalCell.h"
+
+@implementation WYNewsNormalCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
