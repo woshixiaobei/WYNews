76f94050be216dc89da27ab9df2e20ac1a5e773b
manager <manager@qq.com>
2016-06-15 16:57:49 +0800

    设置应用程序图标&启动图片

---
+++ WYNews/WYNews/Class/Main/WYMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  WYMainViewController.h
+//  WYNews
+//
+//  Created by 小贝 on 16/6/15.
+//  Copyright © 2016年 小贝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYMainViewController : UIViewController
+
+@end
