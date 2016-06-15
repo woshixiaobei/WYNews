aa9862cdda29a6244acb23cabbe5e7db0b5684cf
manager <manager@qq.com>
2016-06-15 20:01:53 +0800

    绑定假数据,实现协议方法设置表格数据

--- WYNews/WYNews/Class/Home/NewsList/WYNewsListController.m
+++ WYNews/WYNews/Class/Home/NewsList/WYNewsListController.m
@@ -8,8 +8,10 @@
 
 #import "WYNewsListController.h"
 
-@interface WYNewsListController ()
+static NSString *cellId = @"cellId";
+@interface WYNewsListController ()<UITableViewDelegate,UITableViewDataSource>
 
+@property (nonatomic,weak) UITableView *tableView;
 @end
 
 @implementation WYNewsListController
@@ -17,9 +19,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor redColor];
+    [self setupUI];
 }
 
+#pragma mark-UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+    cell.textLabel.text = @(indexPath.row).description;
+    
+    return cell;
+
+}
+#pragma mark-搭建界面
+- (void)setupUI {
+
+    UITableView *tv = [[UITableView alloc]initWithFrame:CGRectZero style:UITableViewStylePlain];
+    [self.view addSubview:tv];
+    
+    //注册可重用标识符
+    [tv registerClass:[UITableViewCell class] forCellReuseIdentifier:cellId];
+    
+    tv.dataSource = self;
+    tv.delegate = self;
+    
+    [tv mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+
+    
+    //记录成员变量
+    _tableView = tv;
+
+}
 
 
 
